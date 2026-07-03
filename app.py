eeb1f67ae7078cf39063929c13249b69dd463faa
Paramita15101985 <64579281+Paramita15101985@users.noreply.github.com>
2026-07-03 23:15:59 +0530

    Implement customer churn prediction logic
    Added model loading and prediction functionality.

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 import streamlit as st
+import joblib
+import numpy as np
 
 st.title("Customer Churn Prediction App")
 
-st.success("App is working 🎉")
+# load model
+model = joblib.load("churn_model.joblib")
 
-st.write("If you see this, deployment is fixed.")
+st.success("Model loaded successfully ✅")
+
+# simple inputs (we will improve later)
+tenure = st.number_input("Tenure")
+monthly = st.number_input("Monthly Charges")
+total = st.number_input("Total Charges")
+
+if st.button("Predict"):
+    input_data = np.array([[tenure, monthly, total]])
+    prediction = model.predict(input_data)
+
+    if prediction[0] == 1:
+        st.error("Customer will CHURN ❌")
+    else:
+        st.success("Customer will NOT churn ✅")
