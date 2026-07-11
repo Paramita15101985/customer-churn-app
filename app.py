1c1aa17b420eafe0d41b229782eb18af66266792
Paramita15101985 <64579281+Paramita15101985@users.noreply.github.com>
2026-07-11 22:38:55 +0530

    Integrate SHAP for model interpretation and visualization
    Added SHAP analysis for model interpretation and visualizations.

--- app.py
+++ app.py
@@ -23,6 +23,9 @@ model = joblib.load(MODEL_PATH)
 
 st.success("Model loaded successfully ✅")
 
+import shap
+import matplotlib.pyplot as plt
+
 # -----------------------------
 # Input Section
 # -----------------------------
@@ -50,3 +53,30 @@ if st.button("Predict Churn"):
     except Exception as e:
         st.error("Prediction error due to model mismatch.")
         st.write(str(e))
+
+
+explainer = shap.TreeExplainer(model)
+
+sample = np.array([[tenure, monthly, total]])
+
+shap_values = explainer.shap_values(sample)
+
+st.subheader("Model Interpretation (SHAP)")
+
+fig, ax = plt.subplots(figsize=(8,3))
+
+shap.plots.waterfall(
+    shap.Explanation(
+        values=shap_values[1][0],
+        base_values=explainer.expected_value[1],
+        data=sample[0],
+        feature_names=[
+            "Tenure",
+            "Monthly Charges",
+            "Total Charges"
+        ]
+    ),
+    show=False
+)
+
+st.pyplot(fig)
